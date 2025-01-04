d12cbbaf5a393c04e4670b63e003a7ad1010b857
studentshikha <145010111+studentshikha@users.noreply.github.com>
2025-01-04 16:47:26 +0530

    Update page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -47,7 +47,7 @@ const [dates , setDates] = useState("")
   const fetchBookings = async () => {
     const dateStr = format(selectedDate, 'yyyy-MM-dd');
     try {
-      const response = await fetch('https://system-backend-five.vercel.app/api/bookings');
+      const response = await fetch('https://system-backend-five.vercel.app/bookings');
       const data = await response.json();
       const bookedSlotsForDay = data
         .filter((booking: any) => booking.date === dateStr)
@@ -88,7 +88,7 @@ const [dates , setDates] = useState("")
     }
 
     try {
-      const response = await fetch('https://system-backend-five.vercel.app/api/book', {
+      const response = await fetch('https://system-backend-five.vercel.app/book', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
